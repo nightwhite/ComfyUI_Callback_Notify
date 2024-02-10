fc3b8a29afa057fc710ea11bc690f498bae539c4
nightwhite <nightwhite634@gmail.com>
2024-02-10 17:05:10 +0800

    feat: ComfyUI 成功通知回调插件

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .callback_notify import CallBack_Notify_Node
+NODE_CLASS_MAPPINGS = {"成功回调": CallBack_Notify_Node}
+NODE_DISPLAY_NAME_MAPPINGS = {"成功回调": "成功回调"}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ callback_notify.py
@@ -0,0 +1,56 @@
+import requests
+import torch
+from torchvision import transforms
+import numpy as np
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+import io
+import base64
+import json
+
+class CallBack_Notify_Node:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "notify_url": ("STRING", {"default":"https://api.xxx.com/notify"}),
+                "image": ("IMAGE", {"forceInput": True}),				
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("图像",)
+
+    FUNCTION = "test"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "image/callback"
+
+    def test(self, notify_url, image):
+        img = Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+        image_data = io.BytesIO()
+        metadata = PngInfo()
+        img.save(image_data, format='PNG', pnginfo=metadata)
+        image_data_bytes = image_data.getvalue()
+        encoded_image = "data:image/png;base64," + base64.b64encode(image_data_bytes).decode('utf-8')
+        data = {
+            "image": encoded_image
+        }
+        json_data = json.dumps(data)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(notify_url, data=json_data, headers=headers)
+        return (image,)
+
+NODE_CLASS_MAPPINGS = {
+    "callback_notify": CallBack_Notify_Node
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "callback_notify": "callback_notify"
+}
